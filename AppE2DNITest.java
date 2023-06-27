1b9ad55855240c074d38d650ad3a47f390bb28fb
bowserchris <133642309+bowserchris@users.noreply.github.com>
2023-06-27 13:01:24 +0200

    Update AppE2DNITest.java
    corrected test to include parameter tests

--- AppE2DNITest.java
+++ AppE2DNITest.java
@@ -2,68 +2,17 @@ package n1e1to3;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class AppE2DNITest {
 
-	@Test
-	void codeReturnedShouldBeA() {
+	@ParameterizedTest
+	@ValueSource(ints = {12345678, 24341234, 36765432, 49344321, 62456123, 74423615, 85651237, 89859632, 93987541, 95154789})
+	//@ValueSource(strings = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}) 	//able to use various parameters?
+	void codeReturnedShouldBeA(int number) {
 		var codeID = new CalculateID();
-		assertEquals("A", codeID.calculateID(12345678));
-	}
-	
-	@Test
-	void codeReturnedShouldBeB() {
-		var codeID = new CalculateID();
-		assertEquals("B", codeID.calculateID(24341234));
-	}
-	
-	@Test
-	void codeReturnedShouldBeC() {
-		var codeID = new CalculateID();
-		assertEquals("C", codeID.calculateID(36765432));
-	}
-	
-	@Test
-	void codeReturnedShouldBeD() {
-		var codeID = new CalculateID();
-		assertEquals("D", codeID.calculateID(49344321));
-	}
-	
-	@Test
-	void codeReturnedShouldBeE() {
-		var codeID = new CalculateID();
-		assertEquals("E", codeID.calculateID(62456123));
-	}
-	
-	@Test
-	void codeReturnedShouldBeF() {
-		var codeID = new CalculateID();
-		assertEquals("F", codeID.calculateID(74423615));
-	}
-	
-	@Test
-	void codeReturnedShouldBeG() {
-		var codeID = new CalculateID();
-		assertEquals("G", codeID.calculateID(85651237));
-	}
-	
-	@Test
-	void codeReturnedShouldBeH() {
-		var codeID = new CalculateID();
-		assertEquals("H", codeID.calculateID(89859632));
-	}
-	
-	@Test
-	void codeReturnedShouldBeI() {
-		var codeID = new CalculateID();
-		assertEquals("I", codeID.calculateID(93987541));
-	}
-	
-	@Test
-	void codeReturnedShouldBeJ() {
-		var codeID = new CalculateID();
-		assertEquals("J", codeID.calculateID(95154789));
+		assertEquals("A", codeID.calculateID(number));
 	}
 
 }
